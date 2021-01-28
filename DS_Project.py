82d04550570c3ee46c83244647af547fcd4ebae1
Jonathan-Barton <70105857+Jonathan-Barton@users.noreply.github.com>
2021-01-28 15:24:39 -0700

    Test Files
    These are files I made to see if creating the repository worked

---
+++ DS_Project.py
@@ -0,0 +1,19 @@
+#Here we can create our project
+
+from tkinter import *
+
+window = Tk()
+
+window.title("Welcome to our prject")
+
+window.geometry('350x200')
+
+lbl = Label(window, text="NASA")
+
+lbl.grid(column=0, row=0)
+
+btn = Button(window, text="Click if you feel chalinga")
+
+btn.grid(column=1, row=0)
+
+window.mainloop()
