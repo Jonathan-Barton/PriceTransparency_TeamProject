a11c6cef61174fd4ad4af08b56914cefa66340ea
Jonathan-Barton <70105857+Jonathan-Barton@users.noreply.github.com>
2021-03-15 17:52:07 -0600

    Price Transparency CSVs
    I'm pushing some of the CSVs with the price transparency data.

--- DS_Project.py
+++ DS_Project.py
@@ -12,7 +12,7 @@ lbl = Label(window, text="NASA")
 
 lbl.grid(column=0, row=0)
 
-btn = Button(window, text="Click if you feel chalinga")
+btn = Button(window, text="Click if you feel chalinga!")
 
 btn.grid(column=1, row=0)
 
